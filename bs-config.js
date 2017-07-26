074ce26785adae66fc055ec31ecb3963b86fcd50
Oni <augustine.oni@accenturefederal.com>
2017-07-26 16:13:42 -0400

    bs-config not needed for now

--- bs-config.js
+++ bs-config.js
@@ -1,17 +1,17 @@
-var proxy = require('http-proxy-middleware');
+// var proxy = require('http-proxy-middleware');
 
-var apiProxy = proxy('/api', {
-    target: 'http://www.example.org',
-    changeOrigin: true   // for vhosted sites
-});
+// var apiProxy = proxy('/api', {
+//     target: 'http://www.example.org',
+//     changeOrigin: true   // for vhosted sites
+// });
 
-module.exports = {
-    server: {
-        // Start from key `10` in order to NOT overwrite the default 2 middleware provided
-        // by `lite-server` or any future ones that might be added.
-        // Reference: https://github.com/johnpapa/lite-server/blob/master/lib/config-defaults.js#L16
-        middleware: {
-            10: apiProxy
-        }
-    }
-};
+// module.exports = {
+//     server: {
+//         // Start from key `10` in order to NOT overwrite the default 2 middleware provided
+//         // by `lite-server` or any future ones that might be added.
+//         // Reference: https://github.com/johnpapa/lite-server/blob/master/lib/config-defaults.js#L16
+//         middleware: {
+//             10: apiProxy
+//         }
+//     }
+// };
